f9a23e6eab23fa39469433c36a3253ad6eb6cdce
Marwen <marwen.jlassi.2020@gmail.com>
2024-10-05 23:44:44 +0100

    Build solution

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NG5W2ONI.mjs";import"./chunk-LNRUNI7N.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YYPAGF67.mjs";import"./chunk-IKSJVHQR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
