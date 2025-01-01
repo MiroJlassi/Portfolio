a2045044325deb29dc21eeeb3fb7369847b09034
Marwen <marwen.jlassi.2020@gmail.com>
2025-01-01 18:40:56 +0100

    add theme switcher button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,4 @@ import { ThemeManagerService } from "../app/theme-manager.service";
 })
 export class AppComponent {
   title = 'portfolio2';
-  currentTheme = 'dark';
-
-  constructor(private themeService: ThemeManagerService) { 
-    this.themeService.currentTheme.subscribe((theme) => {
-      this.currentTheme = theme;
-    });
-  }
-
-  toggleTheme(): void {
-    const newTheme = this.currentTheme === 'light' ? 'dark' : 'light';
-    this.themeService.setTheme(newTheme);
-  }
 }
