929a9df763124a43b345deeef13ae948980fbe9a
Marwen <marwen.jlassi.2020@gmail.com>
2025-01-25 20:10:09 +0100

    builde prod version 🚀

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{b as a}from"./chunk-RRJRNN2U.mjs";import"./chunk-27UQSHOS.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
+import{b as a}from"./chunk-5Y353FIL.mjs";import"./chunk-ATVRKQOL.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
